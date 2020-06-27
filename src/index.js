03d73d6acf6920b237537e7f92323964999537c6
Arun Kumar <arun.kumar@zoomcar.com>
2020-06-27 23:27:16 +0530

    feat: adds on scroll active nav changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-console.log('initiated')
+const allOptions = document.querySelectorAll('.nav-options a');
+const allSections = document.querySelectorAll('section');
+let navClicked = false;
+
+document.querySelector('.nav-options').addEventListener('click', (event) => {
+  const elem = event.target;
+  navClicked = true;
+  setNavActive(elem);
+  //delay to debounce scroll setNav
+  setTimeout(() => {
+    navClicked = false;
+  }, 1000);
+})
+
+const setNavActive = (element) => {
+  allOptions.forEach((item) => {
+    item.classList.remove('active');
+  })
+
+  element.classList.add('active');
+}
+
+const onLoad = () => {
+  const hash = document.location.hash;
+  if (hash) {
+    setNavActive(document.querySelector(`a[href='${hash}']`))
+  }
+}
+
+
+document.onscroll = () => {
+  if (!navClicked) {
+    allSections.forEach((section) => {
+      if (window.scrollY >= section.offsetTop) {
+        setNavActive(document.querySelector(`a[href='#${section.id}']`))
+      }
+    })
+    if ((window.innerHeight + window.scrollY) + 50 >= document.body.offsetHeight) {
+      setNavActive(document.querySelector(`a[href='#${allSections[allSections.length - 1].id}']`))
+    }
+  }
+}
+
+onLoad();
